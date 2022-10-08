e4078014ff33e69f84a10923a6d30edd2202e6cc
Oksana <oxana.manchuk@mail.ru>
2022-10-08 18:34:20 +0300

    test

---
+++ index.94db852a.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper");new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},speed:1500,autoplay:{delay:1500},breakpoints:{320:{slidesPerview:1},480:{slidesPerView:1},992:{slidesPerview:1}}});
+//# sourceMappingURL=index.94db852a.js.map
---
+++ index.d1531357.js
@@ -0,0 +1,2 @@
+!function(){new Swiper(".swiper");new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},speed:1500,autoplay:{delay:1500},breakpoints:{320:{slidesPerview:1},480:{slidesPerView:1},992:{slidesPerview:1}}})}();
+//# sourceMappingURL=index.d1531357.js.map
